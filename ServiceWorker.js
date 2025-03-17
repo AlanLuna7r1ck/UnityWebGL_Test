d4ce589f8f9cdad8fb3ef744640fef3714b7da1c
Alan Luna <alan.luna@7r1ck.com>
2025-03-17 17:07:55 -0300

    Rebuilt with no compression data

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "DefaultCompany-My Unity 6 Project Test-1.61";
 const contentToCache = [
     "Build/WebGL_Telegram_Builds.loader.js",
-    "Build/WebGL_Telegram_Builds.framework.js.gz",
-    "Build/WebGL_Telegram_Builds.data.gz",
-    "Build/WebGL_Telegram_Builds.wasm.gz",
+    "Build/WebGL_Telegram_Builds.framework.js",
+    "Build/WebGL_Telegram_Builds.data",
+    "Build/WebGL_Telegram_Builds.wasm",
     "TemplateData/style.css"
 
 ];
